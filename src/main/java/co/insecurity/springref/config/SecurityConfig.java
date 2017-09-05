bdbc30e2cfc1a622baa3077a9c38320b287be277
Milo Minderbinder <minderbinder.enterprises@gmail.com>
2017-09-05 15:28:07 -0400

    add SecurityConfig documentation

--- src/main/java/co/insecurity/springref/config/SecurityConfig.java
+++ src/main/java/co/insecurity/springref/config/SecurityConfig.java
@@ -60,6 +60,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                // While configurable at the app container or server level, configuring channel security in the
+                // code itself can ensure that deployment environment issues, like failure to enforce HTTPS site-wide,
+                // will not affect the application's security and will be more easily identified.
                 .requiresChannel()
                     .anyRequest()
                         .requiresSecure()
@@ -71,6 +74,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .hasRole(UserRole.USER.toString())
                     .antMatchers("/", "/register", "/resources/**")
                         .permitAll()
+                    // Adding a catch-all pattern to enforce authentication for any remaining unspecified paths is an
+                    // effective way to ensure that security controls "fail closed" and preventing access to functionality
+                    // and paths that are not explicitly exposed to users/roles
                     .anyRequest()
                         .authenticated()
                     .and()
@@ -96,16 +102,47 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+    /**
+     * Get the {@link PasswordEncoder} bean, which can be used to safely hash credentials for storage.
+     *
+     * Spring Security provides the {@link BCryptPasswordEncoder}, which uses the bcrypt one-way hashing algorithm for
+     * hashing passwords and other credentials. Bcrypt offers security improvements over other hashing functions, like
+     * SHA and even PBKDF2, because passwords hashed with bcrypt are more resistant to rainbow-table attacks, since the
+     * algorithm is intentionally CPU and GPU intensive, making it much more resource/time intensive for an attacker to
+     * generate password hashes from a dictionary.
+     *
+     * @return the {@link BCryptPasswordEncoder}
+     */
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    /**
+     * Get the {@link SimplePasswordPolicy} bean, which can be used to verify that passwords meet policy requirements.
+     *
+     * In this application, the {@link SimplePasswordPolicy} defines example length, character, and uniqueness policy
+     * requirements, and is used by the {@link co.insecurity.springref.web.domain.UserValidator} during registration and
+     * password change/update flows as a centralized point of enforcement for these policies.
+     *
+     * @return the {@link SimplePasswordPolicy}
+     */
     @Bean
     public PasswordPolicy getPasswordPolicy() {
         return new SimplePasswordPolicy(true);
     }
 
+    /**
+     * Get the {@link AuthenticationProvider} bean, used by the {@link AuthenticationManager} to process a specific
+     * {@link org.springframework.security.core.Authentication Authentication} implementation.
+     *
+     * In this configuration, a {@link RateLimitingDaoAuthenticationProvider} is instantiated, which limits the rate at
+     * which a particular username can be used to authenticate successfully from a given IP address. In this case, the
+     * {@link RateLimitingDaoAuthenticationProvider} uses the configured {@link BCryptPasswordEncoder} to hash passwords,
+     * and a {@link UserPersistenceService} to provide simple CRUD operations using JDBC to retrieve user information.
+     *
+     * @return the {@link RateLimitingDaoAuthenticationProvider}
+     */
     @Bean
     public AuthenticationProvider getAuthenticationProvider() {
         RateLimitingDaoAuthenticationProvider authProvider =
